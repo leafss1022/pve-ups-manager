53b7183a817b48cb191465aff3f5f1b53a203423
leaf <118503199+leafss1022@users.noreply.github.com>
2026-07-19 08:32:34 +0800

    v0.6.0: remote NUT, WebSocket push, PVE 8.x CT name fix

--- scripts/pve-ups-shutdown.sh
+++ scripts/pve-ups-shutdown.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+﻿#!/bin/bash
 # PVE UPS Manager - NUT/apcupsd shutdown hook
 # This script safely shuts down all VMs and containers before host shutdown
 # Installed to /usr/local/bin/pve-ups-shutdown
